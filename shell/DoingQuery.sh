c79a2d6319e6a228032554bcd817499df6c4ea35
cwy <1019636041@qq.com>
2017-12-25 21:24:57 +0800

    cwy

--- shell/DoingQuery.sh
+++ shell/DoingQuery.sh
@@ -5,7 +5,7 @@ dir_today="/var/log/DoingQuery/`date +%Y/%m`"
 log_path="/var/log/DoingQuery/`date +%Y/%m/%d`.log"
 /bin/mkdir -p $dir_today > /dev/null 2>&1
 
-count=`/bin/ps -ef | grep '/usr/bin/php' | grep artisan | grep DoingQuery | grep -v grep | grep domain=gwsd.bookuu.com | wc -l`
+count=`/bin/ps -ef | grep '/usr/bin/php' | grep artisan | grep DoingQuery | grep -v grep | wc -l`
 
 # zc=`/bin/ps -ef | grep '/usr/bin/php' | grep artisan | grep DoingQuery  | grep -v grep | grep domain=gwsd.bookuu.com | wc -l`
 # 不杀进程 
